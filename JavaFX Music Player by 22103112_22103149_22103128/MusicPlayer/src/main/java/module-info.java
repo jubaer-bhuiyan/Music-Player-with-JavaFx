58a9d6315138b0f3fe97a33e58d35814e45e8cad
Jubaer Ahamed Bhuiyan <jubaerahamed4444@gmail.com>
2025-07-05 20:11:50 +0600

    Delete JavaFX Music Player by 22103112_22103149_22103128 directory

--- JavaFX Music Player by 22103112_22103149_22103128/MusicPlayer/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.music.musicplayer {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.media;
-
-
-    opens com.music.musicplayer to javafx.fxml;
-    exports com.music.musicplayer;
-}
